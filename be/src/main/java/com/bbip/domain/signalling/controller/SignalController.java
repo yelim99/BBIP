d47e173536913c77cf8160d65467fb57ef751dc6
97choijw <97choijw@gmail.com>
2024-09-30 10:31:17 +0900

    feat: log

--- be/src/main/java/com/bbip/domain/signalling/controller/SignalController.java
+++ be/src/main/java/com/bbip/domain/signalling/controller/SignalController.java
@@ -1,5 +1,6 @@
 package com.bbip.domain.signalling.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -7,23 +8,27 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@Slf4j
 public class SignalController {
 
     @MessageMapping("/cam/{key}")
     @SendTo("/sub/client/{key}")
     public String camToClient(@DestinationVariable("key") String key, @Payload String message) {
+        log.info("message: " + message);
         return message;
     }
 
     @MessageMapping("/client/{key}")
     @SendTo("/sub/cam/{key}")
     public String clientToCam(@DestinationVariable("key") String key, @Payload String message) {
+        log.info("message: " + message);
         return message;
     }
 
     @MessageMapping("/message")
     @SendTo("/sub/message")
     public String signal(String message) {
+        log.info("message: " + message);
         return message;
     }
 }
